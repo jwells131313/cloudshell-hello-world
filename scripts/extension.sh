370850867a203d7f619d8628b328a1afaf22ff96
John Wells <jwells131313@gmail.com>
2020-07-05 09:00:17 -0400

    Add scripts

---
+++ scripts/extension.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hello World!"
+echo "Your host is ${CS_HOST_OCID}"
+echo "Your terminal is ${CS_HOST_OCID}"
+echo "Your home directory is ${CS_EXTENSION_HOME}"
---
+++ scripts/finishExtension.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "The finisher has run for ${CS_TERMINAL_OCID} on "${CS_HOST_OCID}"
